749eb3d0e1b0ab757be02e3c476a5c64aa0e8cdd
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Air.local>
2021-07-28 06:13:19 +0600

    connect between next js and mongodb , create note, view, delete

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  env: {
+      MONGO_URI : "mongodb+srv://sabbir17:9876@cluster0.yc52e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
 }
---
+++ pages/api/notes/index.js
@@ -0,0 +1,35 @@
+import dbConnect from "../../../utilis/dbConnect";
+import Note from '../../../models/Note';
+
+
+
+dbConnect();
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+    const { method } = req;
+
+    switch (method) {
+        case 'GET':
+            try {
+                const notes = await Note.find({});
+
+                res.status(200).json({ success: true, data: notes })
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'POST':
+            try {
+                const note = await Note.create(req.body);
+
+                res.status(201).json({ success: true, data: note })
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+}
